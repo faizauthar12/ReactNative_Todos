8575acdc13fde934eebcc41375d99c9e2715bb75
Faiz Authar <faizauthar@gmail.com>
2021-07-03 15:04:50 +0700

    Todos: Components: add Todo List text

---
+++ src/components/TodoListText.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+
+const TodoListText = () => {
+    return (
+        <Text style={styles.text}>Todo List</Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
+        paddingHorizontal: 26
+    },
+});
+
+export default TodoListText;
